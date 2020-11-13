6823c68c94ed510fe8d14bdb2e3666a709b1f54b
Nick Meinhold <nick.meinhold@gmail.com>
2020-11-14 02:24:31 +1100

    Setup: firebase (functions, firestore, hosting) (#2)

---
+++ functions/src/index.ts
@@ -0,0 +1,6 @@
+import * as functions from 'firebase-functions';
+
+export const test = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
