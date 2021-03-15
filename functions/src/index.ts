9e4db8a844d089a206cbeb2cdface1bb624a645f
Nick Meinhold <nick.meinhold@gmail.com>
2021-03-15 22:05:42 +1100

    Remove firebase functions (#30)

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
-admin.initializeApp();
-
-import { saveDetails } from './auth/on_first_sign_in';
-
-// when a new account is created, add auth details to the database
-export const saveDetailsOnFirstSignIn = functions.auth.user().onCreate(saveDetails);
