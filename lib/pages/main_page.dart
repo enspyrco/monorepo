a5366c4207f4917e5e8be74d5bf2953c053479e7
Nick Meinhold <nick.meinhold@gmail.com>
2021-09-03 22:17:26 +1000

    Update to new redfire with email auth (#2)
    * Add sign out button to MainPage
    
    * Update to latest versions of dependencies

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,10 +1,17 @@
+import 'package:adventures/main.dart';
 import 'package:flutter/material.dart';
+import 'package:redfire/actions.dart';
+import 'package:redfire/extensions.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('MainPage'));
+    return Center(
+        child: ElevatedButton(
+      onPressed: () => context.dispatch<AppState>(SignOutAction()),
+      child: const Text('Sign Out'),
+    ));
   }
 }
