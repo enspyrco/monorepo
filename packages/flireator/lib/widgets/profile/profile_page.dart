0164373e77ec4269bcd1187a602a8a0e1935218b
Nick Meinhold <nick.meinhold@gmail.com>
2021-09-29 23:26:45 +1000

    Add menu button
    - fixed settings.json to exclude all relevant files

--- packages/flireator/lib/widgets/profile/profile_page.dart
+++ packages/flireator/lib/widgets/profile/profile_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redfire/actions.dart';
-import 'package:redfire/extensions.dart';
 import 'package:redfire/types.dart';
 import 'package:redfire/widgets.dart';
 
@@ -23,17 +21,99 @@ class ProfilePage extends StatelessWidget {
                     color: Colors.black, //change your color here
                   ),
                   actions: [
-                MaterialButton(
-                  onPressed: () => context.dispatch<AppState>(SignOutAction()),
-                  child: const Text('Sign Out'),
-                ),
+                const HiddenAccountButtonForPopup(),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ProfileAvatar(userData?.photoURL, onPressed: () {
-                    // context.dispatch(NavigateToProfileAction());
-                  }),
-                )
+                    padding: const EdgeInsets.all(8.0),
+                    child: ProfileAvatar(userData?.photoURL, onPressed: () {})),
               ]));
         });
   }
 }
+
+enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
+
+class HiddenAccountButtonForPopup extends StatelessWidget {
+  const HiddenAccountButtonForPopup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<WhyFarther>(
+      child: Container(),
+      onSelected: (WhyFarther result) {},
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<WhyFarther>>[
+        const PopupMenuItem<WhyFarther>(
+          value: WhyFarther.harder,
+          child: Text('Working a lot harder'),
+        ),
+        const PopupMenuItem<WhyFarther>(
+          value: WhyFarther.smarter,
+          child: Text('Being a lot smarter'),
+        ),
+        const PopupMenuItem<WhyFarther>(
+          value: WhyFarther.selfStarter,
+          child: Text('Being a self-starter'),
+        ),
+        const PopupMenuItem<WhyFarther>(
+          value: WhyFarther.tradingCharter,
+          child: Text('Placed in charge of trading charter'),
+        ),
+      ],
+    );
+  }
+}
+
+
+// class AccountButton extends StatelessWidget {
+//   const AccountButton({
+//     required String? photoUrl,
+//     Key? key,
+//   })  : _photoUrl = photoUrl,
+//         super(key: key);
+
+//   final String? _photoUrl;
+//   final _overlayEntry =
+//       OverlayEntry(builder: (context) => const FloatingMenu());
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(8.0),
+//       child: ProfileAvatar(_photoUrl, onPressed: () {
+//         Overlay.of(context)?.insert();
+//         // context.dispatch(NavigateToProfileAction());
+//       }),
+//     );
+//   }
+// }
+
+// class FloatingMenu extends StatelessWidget {
+//   const FloatingMenu({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Positioned(
+//       right: 10,
+//       top: 10,
+//       child: SizedBox(
+//         width: 150,
+//         child: Material(
+//           child: ListView(
+//             shrinkWrap: true,
+//             children: [
+//               MaterialButton(
+//                   onPressed: () => context.dispatch<AppState>(SignOutAction()),
+//                   child: const Align(
+//                       alignment: Alignment.centerLeft,
+//                       child: Text('Sign Out'))),
+//               MaterialButton(
+//                 onPressed: () {},
+//                 child: const Align(
+//                     alignment: Alignment.centerLeft, child: Text('Cancel')),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
