1df2a6e80f0386f000d9ac0287827d51096a8ecc
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-17 00:02:26 +1100

    Add minimal tests so no coverage files are empty

--- packages/adventures/test/widget_test.dart
+++ packages/adventures/test/widget_test.dart
@@ -1,7 +1,13 @@
+import 'package:adventures/pages/main_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('... test', (WidgetTester tester) async {
-    expect(true, true);
+  testWidgets('MainPage has Sign Out', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: MainPage()));
+
+    final textFinder = find.text('Sign Out');
+
+    expect(textFinder, findsOneWidget);
   });
 }
