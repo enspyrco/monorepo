c16feac5b2b31f26e3b75665674bb259640ebbba
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-25 18:53:56 +1100

    Remove import of missing file

--- packages/redfire/lib/widgets.dart
+++ packages/redfire/lib/widgets.dart
@@ -11,5 +11,4 @@ export 'src/auth/widgets/auth_page_buttons/google_sign_in_button.dart';
 export 'src/profile/widgets/buttons/account_button.dart';
 export 'src/profile/widgets/profile_avatar.dart';
 // shared
-export 'src/shared/widgets/empty_app_bar.dart';
 export 'src/shared/widgets/waiting_indicator.dart';
