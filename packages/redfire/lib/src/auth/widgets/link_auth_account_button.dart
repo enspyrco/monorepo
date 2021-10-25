d43ab66490cf6b594dcf686a63437031ff7a79b3
Nick Meinhold <nick.meinhold@gmail.com>
2021-10-25 20:20:55 +1100

    Add widgets

---
+++ packages/redfire/lib/src/auth/widgets/link_auth_account_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LinkAuthAccountButton extends StatelessWidget {
+  const LinkAuthAccountButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
