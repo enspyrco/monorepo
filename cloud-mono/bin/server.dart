c0c2c968f810e6c03f168f5fa7dc0b5f25405ee8
Nick Meinhold <nick.meinhold@gmail.com>
2021-11-08 10:05:18 +1100

    cloud-mono-Add game sdk lib files & ffigen

--- cloud-mono/bin/server.dart
+++ cloud-mono/bin/server.dart
@@ -1,32 +1,33 @@
+import 'dart:ffi' as ffi;
 import 'dart:io';
+import 'dart:io' show Platform, Directory;
 
+import 'package:path/path.dart' as path;
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
-
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = params(request, 'message');
-  return Response.ok('$message\n');
-}
 
 void main(List<String> args) async {
+  var libraryPath =
+      path.join(Directory.current.path, 'lib', 'discord_game_sdk.so');
+  if (Platform.isMacOS) {
+    libraryPath =
+        path.join(Directory.current.path, 'lib', 'discord_game_sdk.dylib');
+  }
+  final dylib = ffi.DynamicLibrary.open(libraryPath);
+
+  final HelloWorld hello = dylib
+      .lookup<ffi.NativeFunction<hello_world_func>>('hello_world')
+      .asFunction();
+
+  hello();
+
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
-  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
-
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(_handler, ip, port);
+
+  // final server = await serve(_handler, ip, port);
+
   print('Server listening on port ${server.port}');
 }
