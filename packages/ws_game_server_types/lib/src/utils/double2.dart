ccb0a2997da348ef9f147c4cab333c09634d197f
Nick Meinhold <nick.meinhold@gmail.com>
2021-11-13 16:43:59 +1100

    [WST] Make Double2 immutable

--- packages/ws_game_server_types/lib/src/utils/double2.dart
+++ packages/ws_game_server_types/lib/src/utils/double2.dart
@@ -1,8 +1,8 @@
 // A data class for sending pairs of doubles (eg. locations) in messages.
 class Double2 {
-  Double2(this.x, this.y);
-  num x;
-  num y;
+  const Double2(this.x, this.y);
+  final num x;
+  final num y;
 
   Map<String, dynamic> toJson() => <String, num>{'x': x, 'y': y};
 
