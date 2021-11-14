7a27de312624beb77a99bb05dd8d16e37ca94c54
Nick Meinhold <nick.meinhold@gmail.com>
2021-11-14 14:08:22 +1100

    Pass markdown in to TaskDescriptionView
    - also added a Report Issue button in a stack on top of markdown

--- packages/tech_world/lib/challenges/widgets/challenge_stepper.dart
+++ packages/tech_world/lib/challenges/widgets/challenge_stepper.dart
@@ -24,7 +24,8 @@ class _ChallengeStepperState extends State<ChallengeStepper> {
             currentStep: _index,
             controlsBuilder: (BuildContext context, ControlsDetails details) =>
                 const SizedBox(
-                    height: 300, child: TaskDescriptionView(lines: <String>[])),
+                    height: 300,
+                    child: TaskDescriptionView(markdown: _markdownData)),
             type: StepperType.horizontal,
             steps: const [
               Step(title: CircularProgressIndicator(), content: SizedBox()),
@@ -38,3 +39,22 @@ class _ChallengeStepperState extends State<ChallengeStepper> {
             }));
   }
 }
+
+const String _markdownData = '''
+ # Minimal Markdown Test
+ ---
+ [This](https://google.com) is a simple Markdown test. Provide a text string with Markdown tags
+ to the Markdown widget and it will display the formatted output in a
+ scrollable widget.
+
+ ## Section 1
+ Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
+ aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
+ eget est condimentum, vitae porttitor diam ornare.
+
+ ### Subsection A
+ Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
+ venenatis libero. **__Curabitur sem lectus, feugiat eu justo in, eleifend
+ accumsan ante.__** Sed a fermentum elit. Curabitur sodales metus id mi
+ ornare, in ullamcorper magna congue.
+ ''';
--- packages/tech_world/lib/challenges/widgets/task_descritpion_view.dart
+++ packages/tech_world/lib/challenges/widgets/task_descritpion_view.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-
-const String _markdownData = '''
- # Minimal Markdown Test
- ---
- This is a simple Markdown test. Provide a text string with Markdown tags
- to the Markdown widget and it will display the formatted output in a
- scrollable widget.
-
- ## Section 1
- Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
- aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
- eget est condimentum, vitae porttitor diam ornare.
-
- ### Subsection A
- Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
- venenatis libero. **__Curabitur sem lectus, feugiat eu justo in, eleifend
- accumsan ante.__** Sed a fermentum elit. Curabitur sodales metus id mi
- ornare, in ullamcorper magna congue.
- ''';
+import 'package:redfire/actions.dart';
+import 'package:tech_world/utils/extensions/build_context_extensions.dart';
 
 class TaskDescriptionView extends StatelessWidget {
-  const TaskDescriptionView({required List<String> lines, Key? key})
+  const TaskDescriptionView({required this.markdown, Key? key})
       : super(key: key);
+
+  final String markdown;
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Markdown(data: _markdownData));
+    return SafeArea(
+      child: Stack(
+        children: [
+          Markdown(
+            data: markdown,
+            onTapLink: (_, href, __) =>
+                context.dispatch(LaunchUrlAction(url: href ?? '')),
+          ),
+          Positioned(
+              right: 0.0,
+              bottom: 0.0,
+              child: OutlinedButton(
+                  onPressed: () {}, child: const Text('Report Issue')))
+        ],
+      ),
+    );
   }
 }
