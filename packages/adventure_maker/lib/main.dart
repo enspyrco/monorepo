d0f7a869aeb0cbcfd2c3a32c691618e2c0f1afbc
Nick Meinhold <nick@enspyr.co>
2021-12-02 14:30:29 +1100

    Add middleware, firestore rules
    - added create adventure middleware for adventure_maker
    - added tech_world firestore rules for adventures

--- packages/adventure_maker/lib/main.dart
+++ packages/adventure_maker/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:adventure_maker/app_state.dart';
+import 'package:adventure_maker/middleware/create_adventure_middleware.dart';
 import 'package:adventure_maker/pages/main_page.dart';
 import 'package:flutter/widgets.dart';
 import 'package:redfire/widgets.dart';
@@ -6,7 +7,7 @@ import 'package:redfire/widgets.dart';
 void main() => runApp(AppWidget<AppState>(
       initialState: AppState.init(),
       initialActions: const [],
-      middlewares: const [],
+      middlewares: [CreateAdventureMiddleware()],
       reducers: const [],
       pageTransforms: const [],
       title: 'adventure_maker', // optional, defaults to 'Title Note Set'
