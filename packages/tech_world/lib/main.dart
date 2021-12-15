1282234c09817cc46cc3e2919b7586608639a4b0
Nick Meinhold <nick@enspyr.co>
2021-12-15 23:25:01 +1100

    [TW] Update to new dart only firebase config

--- packages/tech_world/lib/main.dart
+++ packages/tech_world/lib/main.dart
@@ -11,6 +11,7 @@ import 'app_state.dart';
 import 'challenges/reducers/dismiss_challenge.dart';
 import 'challenges/reducers/start_challenge_reducer.dart';
 import 'game/tech_world_game.dart';
+import 'redfire_options.dart';
 import 'redux/middleware/set_auth_user_data_middleware.dart';
 import 'redux/reducers/set_other_player_ids_reducer.dart';
 import 'redux/reducers/set_player_path_reducer.dart';
@@ -48,6 +49,7 @@ void main() {
       appStateChanges: store.onChange, serverSink: _serverController.sink);
 
   runApp(AppWidget<AppState>.fromStore(
+      config: RedFireOptions.currentPlatform,
       initializedStore: store,
       title: 'Tech World',
       mainPage: MainPage(game: game)));
