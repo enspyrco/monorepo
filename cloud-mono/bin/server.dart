0d870ee1024672f4aea4615cf8220bba56abad42
Nick Meinhold <nick@enspyr.co>
2022-01-31 13:57:58 +1100

    cloud-mono-Complete a minimal working server for cloud run

--- cloud-mono/bin/server.dart
+++ cloud-mono/bin/server.dart
@@ -1,32 +1,20 @@
-import 'dart:io';
+import 'package:shelf/shelf.dart' show Request, Response;
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
+Future<Response> handler(Request request) async {
+  // Decode the JSON into a push message.
+  final body = await request.readAsString();
 
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  print(body);
+  // var pushMessage = PushEvent.fromJson(body);
+  // print(pushMessage.message.asString);
 
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+  // The subscription requires a 200 response
+  return Response.ok('Success.');
 }
 
-Response _echoHandler(Request request) {
-  final message = params(request, 'message');
-  return Response.ok('$message\n');
-}
-
-void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
-
-  // Configure a pipeline that logs requests.
-  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
-
-  // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(_handler, ip, port);
-  print('Server listening on port ${server.port}');
+void main() {
+  shelf_io.serve(handler, '0.0.0.0', 8080).then((server) {
+    print('Serving at https://${server.address.host}:${server.port}');
+  });
 }
