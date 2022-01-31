7aadfdeec717ebb181544e4c9e2fd924ed0b0fcb
Nick Meinhold <nick@enspyr.co>
2022-01-31 13:57:58 +1100

    cloud-mono-Add eventfire package

---
+++ cloud-mono/packages/eventfire/lib/src/firestore_service.dart
@@ -0,0 +1,30 @@
+import 'package:eventfire/src/extensions/json_map_extension.dart';
+import 'package:googleapis/firestore/v1.dart';
+import 'package:googleapis_auth/googleapis_auth.dart';
+
+import 'utils/typedefs.dart';
+
+/// If [api] is omitted, [client] is used to create one, so [client] must be present.
+/// If [api] is present, [client] can be ommited.
+class FirestoreService {
+  FirestoreService(
+      {required String projectId,
+      AutoRefreshingAuthClient? client,
+      FirestoreApi? api})
+      : _databaseName = 'projects/' + projectId + '/databases/(default)' {
+    _api = api ?? FirestoreApi(client!);
+    _docs = _api.projects.databases.documents;
+  }
+
+  final String _databaseName;
+  late final FirestoreApi _api;
+  late final ProjectsDatabasesDocumentsResource _docs;
+
+  Future<Document> setDocument(
+          {required String at, required JsonMap to}) async =>
+      _docs.createDocument(
+        to.toDocument(),
+        _databaseName + '/documents',
+        at,
+      );
+}
