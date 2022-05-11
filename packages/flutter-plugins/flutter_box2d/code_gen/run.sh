45c32593b4cb3ab4705b4819a73feee328b3ad4f
Nick Meinhold <nick@enspyr.co>
2022-05-12 00:52:49 +1000

    Update code gen script to move generated files
    We have a script that generates bindings into code_gen/out/ then
    copies the relevant files to the package where they live. The generated
    just cause lots of errors where they are because they are out of place
    so the  imports are wrong. I initially added ignore statements to the
    analysis_options but there doesn't seem to be any reason to keep the
    extra copy of the generated files.

--- packages/flutter-plugins/flutter_box2d/code_gen/run.sh
+++ packages/flutter-plugins/flutter_box2d/code_gen/run.sh
@@ -1,10 +1,11 @@
 python3 parse_and_generate.py Box2D.idl
-cp out/b2_adapters_ffi.dart ../flutter_box2d_macos/lib/b2_adapters_ffi.dart
-cp out/b2_adapters_web.dart ../flutter_box2d_web/lib/b2_adapters_web.dart
-cp out/b2_decorators.dart ../flutter_box2d_platform_interface/lib/b2_decorators.dart
-cp out/b2_delegates.dart ../flutter_box2d_platform_interface/lib/b2_delegates.dart
-cp out/b2_enums.dart ../flutter_box2d_platform_interface/lib/b2_enums.dart
-cp out/flutter_box2d_macos.dart ../flutter_box2d_macos/lib/flutter_box2d_macos.dart
-cp out/flutter_box2d_platform_interface.dart ../flutter_box2d_platform_interface/lib/flutter_box2d_platform_interface.dart
-cp out/flutter_box2d_web.dart ../flutter_box2d_web/lib/flutter_box2d_web.dart
+mv out/b2_adapters_ffi.dart ../flutter_box2d_macos/lib/b2_adapters_ffi.dart
+mv out/b2_adapters_web.dart ../flutter_box2d_web/lib/b2_adapters_web.dart
+mv out/b2_decorators.dart ../flutter_box2d_platform_interface/lib/b2_decorators.dart
+mv out/b2_delegates.dart ../flutter_box2d_platform_interface/lib/b2_delegates.dart
+mv out/b2_enums.dart ../flutter_box2d_platform_interface/lib/b2_enums.dart
+mv out/flutter_box2d_macos.dart ../flutter_box2d_macos/lib/flutter_box2d_macos.dart
+mv out/flutter_box2d_platform_interface.dart ../flutter_box2d_platform_interface/lib/flutter_box2d_platform_interface.dart
+mv out/flutter_box2d_web.dart ../flutter_box2d_web/lib/flutter_box2d_web.dart
+rm out/glue.c
 dart format ../flutter_box2d_platform_interface/lib/ ../flutter_box2d_macos/lib/ ../flutter_box2d_web/lib/
