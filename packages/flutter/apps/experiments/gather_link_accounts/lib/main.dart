42e0f8eb01c609ab834f9984258d682fd35e9e30
Nick Meinhold <nick@enspyr.co>
2022-07-20 00:04:29 +1000

    Configure Firebase and run macos target

--- packages/flutter/apps/experiments/gather_link_accounts/lib/main.dart
+++ packages/flutter/apps/experiments/gather_link_accounts/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,9 +29,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text('Home Screen')),
-    ));
+      home: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, AsyncSnapshot<FirebaseApp> snapshot) {
+            if (snapshot.hasError) {
+              // handle error
+            }
+            if (snapshot.hasData) {
+              return Scaffold(
+                appBar: AppBar(),
+                body: StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (context, AsyncSnapshot<User?> snapshot) {
+                      if (snapshot.hasError) {
+                        // handle error
+                      }
+                      if (snapshot.hasData) {
+                        var user = snapshot.data;
+                        if (user == null) {
+                          return const Center(child: Text('Auth Screen'));
+                        }
+                        return const Center(child: Text('Home Screen'));
+                      } else {
+                        return Container();
+                      }
+                    }),
+              );
+            } else {
+              return Container();
+            }
+          }),
+    );
   }
 }
