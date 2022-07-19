aae3b303cf083bbe01d0644417a09edd0cbe1ade
Nick Meinhold <nick@enspyr.co>
2022-07-20 00:04:29 +1000

    Initial commit.

---
+++ packages/flutter/apps/experiments/gather_link_accounts/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gather Link Accounts',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text('Home Screen')),
+    ));
+  }
+}
