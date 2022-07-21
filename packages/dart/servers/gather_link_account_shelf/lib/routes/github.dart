7e1cf6b94ca5866cf0f37bb0ae3026ec9d901f79
Nick Meinhold <nick@enspyr.co>
2022-07-21 22:22:53 +1000

    Add a route for linking github

--- packages/dart/servers/gather_link_account_shelf/lib/routes/github.dart
+++ packages/dart/servers/gather_link_account_shelf/lib/routes/github.dart
@@ -1,36 +1,43 @@
-import 'package:shelf/shelf.dart' show Response, Request;
+import 'package:shelf/shelf.dart' show Request, Response;
 
 import '../services/locate.dart';
 
-Future<Response> gatherHandler(Request request) async {
+// https://gather-link-account-shelf-eogj3aa7na-uc.a.run.app/github/?code=a3ba08bbee674a21a85b&state=lvr_W0cxHHezXKDDhMnT7zbGTGVmGtoTjiB_-OYVNAU%3D
+
+Future<Response> githubHandler(Request request) async {
   final firestore = Locate.firestore;
-  String? nonce, gatherId;
+  String? nonce, code, githubId;
 
   try {
-    nonce = request.url.queryParameters['nonce']!;
-    gatherId = request.url.queryParameters['gatherPlayerId']!;
+    nonce = request.url.queryParameters['state']!;
+    code = request.url.queryParameters['code']!;
 
     var docs = await firestore.getDocuments(
-        at: 'users', where: 'gathernonce', isEqualTo: nonce);
+        at: 'users', where: 'githubnonce', isEqualTo: nonce);
 
     if (docs.length != 1) {
       throw '${docs.length} docs were found with the passed nonce, there should be 1';
     }
 
+    // TODO: Exchange the code & secret for a token
+    // TODO: Use the github API to get the username
+
+    githubId = 'github id';
+
     var doc = docs.first;
     var uid = doc.id;
     var fields = doc.fields;
-    fields['gather'] = gatherId;
-    fields.remove('gathernonce');
+    fields['github'] = githubId;
+    fields.remove('githubnonce');
 
     await firestore.setDocument(at: 'users/$uid', to: fields);
 
-    return Response.ok('Gather account successfully linked!\n\n'
+    return Response.ok('GitHub account successfully linked!\n\n'
         'You can close this window and return to the original window.');
   } catch (error, trace) {
     firestore.createDocument(at: 'errors', from: {
       'nonce': nonce,
-      'gatherId': gatherId,
+      'githubId': githubId,
       'timestamp': DateTime.now().millisecondsSinceEpoch,
       'error': error.toString(),
       'trace': trace.toString()
--- packages/dart/servers/gather_link_account_shelf/lib/routes/root.dart
+++
@@ -1,5 +0,0 @@
-import 'package:shelf/shelf.dart' show Request, Response;
-
-Response rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
