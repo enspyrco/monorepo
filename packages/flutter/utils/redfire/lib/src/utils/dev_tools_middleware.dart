1dc8412281f73fa5e89cc8839b913c7dd1bf10b6
Nick Meinhold <nick@enspyr.co>
2022-08-21 00:58:55 +1000

    feat(rfr): Add Redux DevTools Screen (#146)
    The redux_devtools_screen package was extracted from the Redux DevTools
    Plugin, which was being developed in a fork of the Flutter DevTools
    (as the plugin architecture has not arrived just yet).  This was done so
    we can have a common package shared by the plugin and redfire.
    
    In redfire we put the ReduxDevToolsScreen alongside the MainScreen with
    a SplitView so the DevTools is visible while we interact with the app.
    
    I added a global stream controller which is used in the
    DevToolsMiddleware (to send events) and in the ReduxDevToolsScreen
    (to receive events). In a future PR I'll add our new locate package and
    and change the global to be in a service locator.

--- packages/flutter/utils/redfire/lib/src/utils/dev_tools_middleware.dart
+++ packages/flutter/utils/redfire/lib/src/utils/dev_tools_middleware.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
 
 import 'package:redux/redux.dart';
+import 'package:redux_devtools_screen/redux_devtools_screen.dart';
 
 import '../types/red_fire_state.dart';
 import '../types/redux_action.dart';
+import 'devtools_global.dart';
 
 class DevToolsMiddleware<T extends RedFireState>
     extends TypedMiddleware<T, ReduxAction> {
@@ -11,13 +13,30 @@ class DevToolsMiddleware<T extends RedFireState>
       : super((store, action, next) async {
           next(action);
 
-          postEvent('redfire:action_dispatched', <String, Object?>{
+          final className = action.toString();
+          String actionName = className[0];
+          int i = 1;
+          while (i < className.length) {
+            if (className[i] == '(') break;
+            if (className[i] == className[i].toUpperCase()) actionName += ' ';
+            actionName += className[i];
+            i++;
+          }
+          actionName = actionName.substring(0, actionName.length - 7);
+
+          var data = <String, Object?>{
             'state_json': store.state.toJson(),
-            'action': {
-              'description': action.toString().split('(').first,
-              'json': action.toJson()
-            }
-          });
+            'action': {'description': actionName, 'json': action.toJson()}
+          };
+
+          // Post an event with state change information that our
+          // Flutter DevTools plugin can listen for.
+          postEvent('redfire:action_dispatched', data);
+
+          // Add an event with state change information to the stream that
+          // we pass in to the ReduxDevToolsScreen, in AppWidget.
+          reduxEventsController.add(
+              ReduxStateEvent(data: data, type: 'redfire:action_dispatched'));
         }) {
     // When the middleware is created, send an event to clear data from devtools.
     // This is useful when user performs a hot restart.
