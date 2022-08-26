2b941769e0c791e30ce6215198aee2e410264a66
Nick Meinhold <nick@enspyr.co>
2022-08-26 15:24:12 +1000

    new(jst): Add the json_types package
    This is just the refactored json_util package - the next PR has
    details on why and how, this PR is to put up a package so
    version solving finds a package when it looks in the git path,
    which seems to happen when a dependency in turn uses a dependency
    with a git path, even when there is an override. The path dep does
    seem to eventually be used but we need a package in the github repo
    to keep the solver happy.

---
+++ packages/dart/utils/json_types/test/json_types_test.dart
@@ -0,0 +1,14 @@
+import 'package:json_types/json_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(true, true);
+    });
+  });
+}
