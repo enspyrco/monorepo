e98c760ac6562858d82b7038eb9ce648348f62fe
Nick Meinhold <nick@enspyr.co>
2022-09-02 18:28:18 +1000

    Add lineage arrow by adding shapes to each item
    I updated the UI for the ActionsHistoryItem to match the designs and
    added drawing of the LineageShape, which is an the arrow that shows
    the lineage of the selected action, so we can easily see how the
    selected action came to be.
    
    I moved the logic of selecting the widgets to show for lineage out of
    the build and instead have each enum hold the relevant widgets. The
    result is a lot cleaner, more readable code.

--- packages/flutter/utils/redux_devtools_screen/lib/src/action_history/actions_history_view.dart
+++ packages/flutter/utils/redux_devtools_screen/lib/src/action_history/actions_history_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:json_types/json_types.dart';
 
 import '../models/dispatch_events.dart';
 import 'action_history_item.dart';
@@ -12,22 +11,31 @@ class ActionsHistoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      child: ListView.builder(
-          itemCount: _dispatchEvents.events.length,
-          itemBuilder: (context, index) {
-            final actionMap =
-                _dispatchEvents.events[index]['action'] as JsonMap?;
-            if (actionMap == null) {
-              return Container();
-            }
-            return ActionHistoryItem(
-                currentlySelected: index == _dispatchEvents.selectedIndex,
-                actionName: actionMap['description'] as String,
-                actionState: actionMap['json'] as JsonMap,
-                onTap: () => _dispatchEvents.setSelected(index));
-          }),
+    return Column(
+      children: [
+        const SizedBox(height: 5), // so starting space is same as the gaps
+        Expanded(
+          child: SizedBox(
+            width: 300,
+            child: ListView.builder(
+                itemCount: _dispatchEvents.events.length,
+                itemBuilder: (context, index) {
+                  final actionData = _dispatchEvents.events[index]['action'];
+                  if (actionData == null) {
+                    return Container();
+                  }
+                  return ActionHistoryItem(
+                    isSelected: index == _dispatchEvents.selectedIndex,
+                    actionName: actionData['name_'],
+                    actionType: actionData['type_'],
+                    actionState: actionData['state_'],
+                    onTap: () => _dispatchEvents.setSelected(index),
+                    lineage: _dispatchEvents.lineageFor[index],
+                  );
+                }),
+          ),
+        )
+      ],
     );
   }
 }
--- packages/flutter/utils/redux_devtools_screen/lib/src/enums/lineage_shape.dart
+++ packages/flutter/utils/redux_devtools_screen/lib/src/enums/lineage_shape.dart
@@ -1,7 +1,128 @@
+import 'package:flutter/material.dart';
+
 enum LineageShape {
-  none,
-  origin,
-  inBetween,
-  onTop,
-  endpoint,
+  origin([OriginShape()]),
+  connection([ConnectionShape()]),
+  notConnection([NotConnectedShapeTop(), NotConnectedShapeBottom()]),
+  endpoint([EndpointShapeTop(), EndpointShapeBottom()]);
+
+  const LineageShape(this.shapeWidgets);
+
+  final List<Widget> shapeWidgets;
+}
+
+class OriginShape extends StatelessWidget {
+  const OriginShape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        right: 30,
+        top: 40,
+        child: Container(
+          width: 20,
+          height: 80,
+          color: Colors.green,
+        ));
+  }
+}
+
+class NotConnectedShapeTop extends StatelessWidget {
+  const NotConnectedShapeTop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 30,
+      child: Container(
+        width: 20,
+        height: 5,
+        color: Colors.green,
+      ),
+    );
+  }
+}
+
+class NotConnectedShapeBottom extends StatelessWidget {
+  const NotConnectedShapeBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 30,
+      bottom: 0,
+      child: Container(
+        width: 20,
+        height: 5,
+        color: Colors.green,
+      ),
+    );
+  }
+}
+
+class ConnectionShape extends StatelessWidget {
+  const ConnectionShape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 30,
+      child: Container(
+        width: 20,
+        height: 80,
+        color: Colors.green,
+      ),
+    );
+  }
+}
+
+class EndpointShapeTop extends StatelessWidget {
+  const EndpointShapeTop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 30,
+      child: Container(
+        width: 20,
+        height: 10,
+        color: Colors.green,
+      ),
+    );
+  }
+}
+
+class EndpointShapeBottom extends StatelessWidget {
+  const EndpointShapeBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        right: 15,
+        top: 10,
+        child: ClipPath(
+          clipper: CustomTriangleClipper(),
+          child: Container(
+            width: 50,
+            height: 50,
+            color: Colors.green,
+          ),
+        ));
+  }
+}
+
+class CustomTriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width / 2, size.height / 2);
+    path.lineTo(0, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
 }
