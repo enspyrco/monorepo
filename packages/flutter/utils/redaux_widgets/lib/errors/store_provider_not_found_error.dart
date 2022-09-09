e4d49ccd317dce47b4742848ca316724397b783f
Nick Meinhold <nick@enspyr.co>
2022-09-09 21:18:24 +1000

    Improve message in StoreProviderNotFoundError

--- packages/flutter/utils/redaux_widgets/lib/errors/store_provider_not_found_error.dart
+++ packages/flutter/utils/redaux_widgets/lib/errors/store_provider_not_found_error.dart
@@ -7,9 +7,12 @@ class StoreProviderNotFoundError<S extends State> extends Error {
   String toString() {
     return '''Error: No StoreProvider<$S> found. Try:
           
-  * Put StoreProvider<$S> above MaterialApp
+  * Move the StoreProvider<$S> higher in the widget tree, above
+    where it is first used (eg. by a context.dispatch call)
   * Provide full type information to Store<$S>, StoreProvider<$S>
-    and StoreConnector<$S, VM>
+    and StateStreamBuilder<$S, VM>
+  * Move the StoreProvider<$S> above MaterialApp if the Navigator
+    may be changing the widget tree, taking out the StoreProvider<$S>
 ''';
   }
 }
