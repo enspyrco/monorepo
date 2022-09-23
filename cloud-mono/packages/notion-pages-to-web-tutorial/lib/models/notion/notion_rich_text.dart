4fa08ad836ea6321166ea6c2852b91d9774aa011
Nick Meinhold <nick@enspyr.co>
2022-09-23 23:36:45 +1000

    chore(+): re/move packages from the cloud monorepo (#181)
    I moved process_articles_in_emails & run_dart_code_in_pubsub_message
    from the 'cloud' monorepo to the servers folder as both are being worked
    on (or pan to be).
    
    I deleted notion_pages_to_web_tutorial as the work done there has been
    superseded by notion_api_client.

--- cloud-mono/packages/notion-pages-to-web-tutorial/lib/models/notion/notion_rich_text.dart
+++
@@ -1,7 +0,0 @@
-import 'package:notion_pages_to_web_tutorial/models/notion/notion_block.dart';
-
-import '../../utils.dart';
-
-class NotionRichText extends NotionBlock {
-  NotionRichText.fromJson(JsonMap json) : super(json);
-}
