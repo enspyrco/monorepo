4bc8970ab8059fab49b947f527d081e9876d9bd6
Nick Meinhold <nick@enspyr.co>
2022-10-01 22:38:01 +1000

    Update BaseErrorHandlers to take a type parameter (#191)
    I also updated the handleLaunchError & handleLandingError functions to
    take useful parameters for handling errors.

--- packages/dart/interfaces/astro_error_handling_interface/lib/astro_error_handling_interface.dart
+++ packages/dart/interfaces/astro_error_handling_interface/lib/astro_error_handling_interface.dart
@@ -2,7 +2,15 @@ library astro_error_handling_interface;
 
 import 'package:astro_state_interface/astro_state_interface.dart';
 
-mixin BaseErrorHandlers {
-  AstroState handleLaunchError(AstroState state);
-  AstroState handleLandingError(AstroState state);
+mixin BaseErrorHandlers<S extends AstroState> {
+  AstroState handleLaunchError(
+      {required Object thrown,
+      required StackTrace trace,
+      required dynamic mission,
+      required S state});
+  AstroState handleLandingError(
+      {required Object thrown,
+      required StackTrace trace,
+      required dynamic mission,
+      required S state});
 }
