346ae8a671a18525da14069503cf6fe437da2138
Nick Meinhold <nick@enspyr.co>
2022-10-01 20:15:35 +1000

    chore(aei): Update with BaseErrorHandlers mixin (#190)
    I added a mixin that error handling plugins will use to
    create a class that can be added to the MissionControl.
    
    Currently the analyzer is happy but we get compiler errors
    suggesting the interface types aren't available. I'm wondering
    if it's due to a problem with the pubspec_overrides so this is also
    a test to see if having the correct types in the main branch will fix
    the problem.

--- packages/dart/interfaces/astro_error_handling_interface/lib/astro_error_handling_interface.dart
+++ packages/dart/interfaces/astro_error_handling_interface/lib/astro_error_handling_interface.dart
@@ -1,1 +1,8 @@
 library astro_error_handling_interface;
+
+import 'package:astro_state_interface/astro_state_interface.dart';
+
+mixin BaseErrorHandlers {
+  AstroState handleLaunchError(AstroState state);
+  AstroState handleLandingError(AstroState state);
+}
--- packages/dart/interfaces/astro_error_handling_interface/lib/src/error_message_base.dart
+++
@@ -1,6 +0,0 @@
-/// Class for carrying basic error information for display to the user.
-abstract class ErrorMessageBase {
-  ErrorMessageBase({required this.message, required this.trace});
-  final String message;
-  final String trace;
-}
