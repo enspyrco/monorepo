2fdea931e6bcaa60369a19029f995b04d284f770
Nick Meinhold <nick@enspyr.co>
2022-10-17 15:20:41 +1100

    new(pdg): Add package_dependency_graph (#202)
    I added a Flutter app to visualise dependencies between a set of Dart
    packages, including showing cycles and highlighting sections of the
    graph. This is just an experiment for now but if useful will probably
    become part of the_process.

---
+++ packages/flutter/apps/experiments/package_dependency_graph/lib/app/state/app_state.dart
@@ -0,0 +1,47 @@
+import 'package:astro_auth/astro_auth.dart';
+import 'package:astro_error_handling/astro_error_handling.dart';
+import 'package:astro_navigation/astro_navigation.dart';
+import 'package:astro_types/state_types.dart';
+
+class AppState
+    with
+        AstroState,
+        DefaultErrorHandlingState,
+        DefaultAuthState,
+        DefaultNavigationState {
+  AppState({required this.error, required this.auth, required this.navigation});
+
+  static AppState get initial => AppState(
+      error: ErrorHandlingState.initial,
+      auth: AuthState.initial,
+      navigation: NavigationState.initial);
+
+  @override
+  final AuthState auth;
+
+  @override
+  final ErrorHandlingState error;
+
+  @override
+  final NavigationState navigation;
+
+  @override
+  AppState copyWith({
+    ErrorHandlingState? error,
+    AuthState? auth,
+    NavigationState? navigation,
+  }) {
+    return AppState(
+      error: error ?? this.error,
+      auth: auth ?? this.auth,
+      navigation: navigation ?? this.navigation,
+    );
+  }
+
+  @override
+  toJson() => {
+        'auth': auth.toJson(),
+        'error': error.toJson(),
+        'navigation': navigation.toJson(),
+      };
+}
