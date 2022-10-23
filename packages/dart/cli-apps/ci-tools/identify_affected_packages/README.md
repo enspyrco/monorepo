85eb9e595ba01180db9f3450a6980ed87e19e982
Nick Meinhold <nick@enspyr.co>
2022-10-23 14:06:21 +1100

    fix(iap): Add functionality, improve iterating (#208)
    The app wasn't saving out a new version of the matrix json,
    now it does.
    
    Iterating over the package paths and changed files paths is still a
    naieve approach but more efficient, baby steps.

--- packages/dart/cli-apps/ci-tools/identify_affected_packages/README.md
+++ packages/dart/cli-apps/ci-tools/identify_affected_packages/README.md
@@ -2,4 +2,4 @@
 
 *A Dart CLI app to identify which packages need testing during CI.*
 
-The app uses git 
+The app reads in the set of all packages (saved in .github/matrix.json), calls git diff via Process.run to get the paths of the changed files, checks those paths against the set of all packages and writes out an adjusted matrix.json so that the tests still run in parallel.
--- packages/dart/cli-apps/ci-tools/identify_affected_packages/bin/identify_affected_packages.dart
+++ packages/dart/cli-apps/ci-tools/identify_affected_packages/bin/identify_affected_packages.dart
@@ -4,12 +4,17 @@ import 'dart:io';
 import 'package:path/path.dart' as path;
 
 void main(List<String> arguments) async {
-  var jsonString = File('.github/matrix.json').readAsStringSync();
+  // read in json describing all packages and build convenience data structures
+  var matrixFile = File('.github/matrix.json');
+  var jsonString = matrixFile.readAsStringSync();
   var decodedJson = jsonDecode(jsonString) as Map<String, dynamic>;
   List<Map<String, dynamic>> packagesJson =
       (decodedJson['include'] as List<dynamic>).cast<Map<String, dynamic>>();
   List<String> packagePaths =
       packagesJson.map((e) => e['path'] as String).toList();
+  Map<String, dynamic> pathMap = {for (var e in packagesJson) e['path']: e};
+
+  /// Use git to determine the paths of all changed files for the current PR
 
   ProcessResult result = await Process.run('git', [
     'diff',
@@ -21,16 +26,22 @@ void main(List<String> arguments) async {
     stderr.write(result.stderr);
     exit(1);
   }
-  // stdout.write(result.stdout);
+
   var pathNames = (result.stdout as String).split('\n');
 
-  List<String> affected = [];
-
-  for (var pathName in pathNames) {
-    for (var packagePath in packagePaths) {
+  // remove packages that are not affected by the changes
+  for (var packagePath in packagePaths) {
+    bool found = false;
+    for (var pathName in pathNames) {
       if (path.isWithin(packagePath, pathName)) {
-        affected.add(packagePath);
+        found = true;
+        break;
       }
     }
+    if (!found) pathMap[packagePath] = null;
   }
+
+  // write out the matrix file, minus the unaffected packages
+  var updatedJson = <String, dynamic>{'include': pathMap.values};
+  matrixFile.writeAsStringSync(jsonEncode(updatedJson));
 }
