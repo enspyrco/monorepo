618462fe0616608d94564c904a5c8806bbc5d17c
Nick Meinhold <nick@enspyr.co>
2022-11-07 16:09:24 +1100

    Fix bug in test, rename tests to astro naming

--- packages/flutter/apps/adventuring/tech_world/test/unit-tests/missions/set_other_player_ids_test.dart
+++ packages/flutter/apps/adventuring/tech_world/test/unit-tests/missions/set_other_player_ids_test.dart
@@ -8,7 +8,7 @@ void main() {
       const testIds = {'1', '2'};
 
       final initialState = AppState.initial;
-      expect(initialState.auth.user, null);
+      expect(initialState.auth.user.uid, null);
 
       const mission = SetOtherPlayerIds(testIds);
       var newState = mission.landingInstructions(initialState);
