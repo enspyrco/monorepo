2fef111f4c738fe5bb16d1960ded44da3b193816
Nick Meinhold <nick@enspyr.co>
2022-11-07 15:41:49 +1100

    Add tech_world_challenge_ui app
    Just an empty app for now, I will next move the relevant code over from
    tech_world.

---
+++ packages/flutter/apps/experiments/tech_world_challenge_ui/test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tech_world_challenge_ui/main.dart';
+
+void main() {
+  testWidgets('...', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(true, isTrue);
+  });
+}
