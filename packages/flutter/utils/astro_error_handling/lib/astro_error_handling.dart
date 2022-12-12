3dc5ce822e6f69eb0387b19014245be7db0f91b6
Nick Meinhold <nick@enspyr.co>
2022-12-12 16:00:37 +1100

    feat(aeh): Add configurable error reports
    I added an ErrorReportSettings member to ErrorReport and
    DefaultErrorHandlers now requires an ErrorReportSettings parameter
    in handleLandingError & handleLaunchError so that an exception can
    tell the handlers how it wants to be displayed.
    
    I created AstroException with members for message and reportSettings so
    that anyone can easily create an exception that configures the resulting
    error message in different ways.
    
    I added AstroInfoException that configures the error message to just
    display the message with no stack trace or mission details. This is
    intended to be used for things like incorrect (but expected) user input
    where we want to give the user info about what happened but not details
    like the stack trace that look like a crash.
    
    The CreateErrorReport landing mission now uses the ErrorReportSettings
    to configure the message and we also pass the ErrorReportSettings in to
    the DefaultErrorReport so the ErrorReportScreen can decide what to
    display (eg. how to display title & stack trace).

--- packages/flutter/utils/astro_error_handling/lib/astro_error_handling.dart
+++ packages/flutter/utils/astro_error_handling/lib/astro_error_handling.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'src/routes/error_report/error_report_page_state.dart';
 import 'src/routes/error_report/error_report_screen.dart';
 
+export 'src/exceptions/astro_exception.dart';
 export 'src/implementations/error_handlers_implementation/default_error_handlers.dart';
 export 'src/missions/create_error_report.dart';
 export 'src/routes/error_report/error_report_page.dart';
--- packages/flutter/utils/astro_error_handling/lib/src/state/models/error_report.dart
+++ packages/flutter/utils/astro_error_handling/lib/src/state/models/error_report.dart
@@ -4,7 +4,12 @@ import 'package:astro_types/state_types.dart';
 
 /// Class for carrying basic error information for display to the user.
 class DefaultErrorReport implements ErrorReport, AstroState {
-  const DefaultErrorReport({required this.message, this.trace, this.details});
+  const DefaultErrorReport({
+    required this.message,
+    this.settings = ErrorReportSettings.fullReport,
+    this.trace,
+    this.details,
+  });
 
   @override
   final String message;
@@ -12,19 +17,32 @@ class DefaultErrorReport implements ErrorReport, AstroState {
   final String? trace;
   @override
   final Map<String, String>? details;
+  @override
+  final ErrorReportSettings settings;
 
   @override
-  DefaultErrorReport copyWith({String? message, String? trace}) =>
+  DefaultErrorReport copyWith(
+          {String? message, String? trace, ErrorReportSettings? settings}) =>
       DefaultErrorReport(
-          message: message ?? this.message, trace: trace ?? this.trace);
+        message: message ?? this.message,
+        trace: trace ?? this.trace,
+        settings: settings ?? this.settings,
+      );
 
   @override
-  JsonMap toJson() => <String, dynamic>{'message': message, 'trace': trace};
+  JsonMap toJson() => <String, dynamic>{
+        'message': message,
+        'trace': trace,
+        'settings': settings,
+      };
 
   @override
   bool operator ==(Object other) =>
-      other is ErrorReport && other.message == message && other.trace == trace;
+      other is DefaultErrorReport &&
+      other.message == message &&
+      other.trace == trace &&
+      other.settings == settings;
 
   @override
-  int get hashCode => Object.hash(message, trace);
+  int get hashCode => Object.hash(message, trace, settings);
 }
