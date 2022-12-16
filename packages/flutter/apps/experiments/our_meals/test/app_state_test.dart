56fefdb30d2917294f5548e41daf9a1c6ad3e6ed
Nick Meinhold <nick@enspyr.co>
2022-12-16 16:43:35 +1100

    feat(oml): add astro_generators
    We can now add state classes with a little boilerplate (via a snippet)
    and have the rest of the class generated via source gen.

--- packages/flutter/apps/experiments/our_meals/test/app_state_test.dart
+++ packages/flutter/apps/experiments/our_meals/test/app_state_test.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:our_meals/app/state/app_state.dart';
-import 'package:test/test.dart';
 
 void main() {
   test('AppState serializes appropriately', () {
