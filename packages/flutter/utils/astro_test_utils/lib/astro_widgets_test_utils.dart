0703f3cb08f50d33cbefa55db718c27082385feb
Nick Meinhold <nick@enspyr.co>
2023-01-11 00:43:07 +1100

    astro_test_utils
    In order for astro_test_utils to provide a test double of UserAuthState
    we:
    - Add code generation capability to package - ie. add astro_generators &
    build_runner dependencies.
    - Add user_auth_state.dart and generate implementation.
    - Export the part class of the generated implementation.

--- packages/flutter/utils/astro_test_utils/lib/astro_widgets_test_utils.dart
+++ packages/flutter/utils/astro_test_utils/lib/astro_widgets_test_utils.dart
@@ -3,6 +3,7 @@ library astro_test_utils;
 export 'src/recording_mission_control.dart';
 export 'src/services/stubbed_firestore_service.dart';
 export 'src/widget_test_harness.dart';
+export 'src/state/user_auth_state.dart';
 
 class AstroException implements Exception {
   AstroException(this.message);
