8ed898904b24209801b9ef7f909ff2f84a49ad05
Nick Meinhold <nick@enspyr.co>
2023-01-10 23:51:51 +1100

    our_meals
    Create a test double for UserAuthState

--- packages/flutter/apps/experiments/our_meals/test/generated/user_auth_state.dart
+++ packages/flutter/apps/experiments/our_meals/test/generated/user_auth_state.dart
@@ -3,7 +3,7 @@
 import 'package:astro_types/astro_annotations.dart';
 import 'package:astro_types/auth_types.dart';
 
-part 'auth_state.g.dart';
+part 'user_auth_state.g.dart';
 
 @GeneratedImplementation(classNameSuffix: 'TestDouble')
-abstract class _AuthState implements AuthState {}
+abstract class _UserAuthState implements UserAuthState {}
