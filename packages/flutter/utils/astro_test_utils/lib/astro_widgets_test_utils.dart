accb5cdcec355e606030feb2e197621e1b5ddf22
Nick Meinhold <nick@enspyr.co>
2023-01-12 20:18:11 +1100

    astro_test_utils
    Add a generated AuthStateTestDouble.

--- packages/flutter/utils/astro_test_utils/lib/astro_widgets_test_utils.dart
+++ packages/flutter/utils/astro_test_utils/lib/astro_widgets_test_utils.dart
@@ -4,6 +4,7 @@ export 'src/recording_mission_control.dart';
 export 'src/services/stubbed_firestore_service.dart';
 export 'src/widget_test_harness.dart';
 export 'src/state/user_auth_state.dart';
+export 'src/state/auth_state.dart';
 
 class AstroException implements Exception {
   AstroException(this.message);
