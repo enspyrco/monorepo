fa614fa00f9db55a01a63da0e0f587342c0a36d3
Nick Meinhold <nick@enspyr.co>
2023-01-12 20:29:29 +1100

    adventure_maker
    Fix another test.

--- packages/flutter/apps/adventuring/adventure_maker/test/shared/middleware/select_adventure_node_test.dart
+++ packages/flutter/apps/adventuring/adventure_maker/test/shared/middleware/select_adventure_node_test.dart
@@ -1,11 +1,11 @@
-import 'package:adventure_maker/_app/state/app_state.dart';
 import 'package:adventure_maker/adventures/models/adventure_model.dart';
 import 'package:adventure_maker/shared/missions/select_adventure_node.dart';
 import 'package:astro_locator/astro_locator.dart';
 import 'package:astro_test_utils/astro_widgets_test_utils.dart';
-import 'package:astro_types/auth_types.dart';
 import 'package:firestore_service_interface/firestore_service_interface.dart';
 import 'package:test/test.dart';
+
+import '../../state/app_state_test_doubles.dart';
 
 void main() {
   group('SelectAdventureNodeMiddleware', () {
@@ -13,10 +13,6 @@ void main() {
       // Create test-doubles.
       var selection = const AdventureModel(name: 'a node name');
       var mission = SelectAdventureNode(selection);
-      var state = AppState.initial;
-      var signedInAppState = state.copyWith(
-          auth: state.auth.copyWith(
-              user: UserAuthStateTestDouble(signedIn: SignedInState.signedIn)));
       var missionControl = RecordingMissionControl(state: signedInAppState);
 
       // Provide stubbed service test-double.
