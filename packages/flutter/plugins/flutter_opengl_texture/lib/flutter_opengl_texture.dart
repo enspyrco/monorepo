0de64446006de0b47222902e8c4316a55983c0da
Nick Meinhold <nick@enspyr.co>
2023-01-15 19:36:55 +1100

    feat(fot): Add native OpenGL code, Texture widget

--- packages/flutter/plugins/flutter_opengl_texture/lib/flutter_opengl_texture.dart
+++ packages/flutter/plugins/flutter_opengl_texture/lib/flutter_opengl_texture.dart
@@ -1,7 +1,25 @@
+import 'package:flutter/services.dart';
 
 import 'flutter_opengl_texture_platform_interface.dart';
 
 class FlutterOpenglTexture {
+  static const MethodChannel _channel = MethodChannel('opengl_texture');
+
+  int? textureId;
+
+  Future<int?> initialize(double width, double height) async {
+    textureId = await _channel.invokeMethod('create', {
+      'width': width,
+      'height': height,
+    });
+    return textureId;
+  }
+
+  Future<void> dispose() =>
+      _channel.invokeMethod('dispose', {'textureId': textureId});
+
+  bool get isInitialized => textureId != null;
+
   Future<String?> getPlatformVersion() {
     return FlutterOpenglTexturePlatform.instance.getPlatformVersion();
   }
