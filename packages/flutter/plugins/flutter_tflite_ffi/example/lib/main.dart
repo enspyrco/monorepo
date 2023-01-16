705cf26318b16e4935d88bfa3c20d07369e0adc6
Nick Meinhold <nick@enspyr.co>
2023-01-16 22:53:55 +1100

    Update example to get TFLite version from plugin

--- packages/flutter/plugins/flutter_tflite_ffi/example/lib/main.dart
+++ packages/flutter/plugins/flutter_tflite_ffi/example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter_tflite_ffi/flutter_tflite_ffi.dart' as flutter_tflite_ffi;
+import 'package:flutter_tflite_ffi/flutter_tflite_ffi.dart'
+    as flutter_tflite_ffi;
 
 void main() {
   runApp(const MyApp());
@@ -11,64 +11,26 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> sumAsyncResult;
+  String _version = '?';
 
   @override
   void initState() {
     super.initState();
-    sumResult = flutter_tflite_ffi.sum(1, 2);
-    sumAsyncResult = flutter_tflite_ffi.sumAsync(3, 4);
+    _version = 'TFLite version: ${flutter_tflite_ffi.version()}';
   }
 
   @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Native Packages'),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                Text(
-                  'sum(1, 2) = $sumResult',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue =
-                        (value.hasData) ? value.data : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: TextAlign.center,
-                    );
-                  },
-                ),
-              ],
-            ),
+          appBar: AppBar(
+            title: const Text('Native Packages'),
           ),
-        ),
-      ),
+          body: Text(_version)),
     );
   }
 }
