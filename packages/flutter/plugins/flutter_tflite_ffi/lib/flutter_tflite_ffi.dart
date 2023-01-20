b21f0c1f2d189ac71c46b87102a928762b05b120
Nick Meinhold <nick@enspyr.co>
2023-01-20 21:08:33 +1100

    Add a function to read the input & output metadata

--- packages/flutter/plugins/flutter_tflite_ffi/lib/flutter_tflite_ffi.dart
+++ packages/flutter/plugins/flutter_tflite_ffi/lib/flutter_tflite_ffi.dart
@@ -2,12 +2,29 @@ import 'dart:ffi';
 import 'dart:io';
 
 import 'package:ffi/ffi.dart';
+import 'package:flutter_tflite_ffi/src/extensions.dart';
 
-import 'flutter_tflite_ffi_bindings_generated.dart';
+import 'src/flutter_tflite_ffi_bindings_generated.dart';
 
 const String _libName = 'libtensorflowlite_c';
 
 String version() => _bindings.TfLiteVersion().cast<Utf8>().toDartString();
+
+String getTensorInputAndOutputCount(String modelPath) {
+  Pointer<TfLiteModel> model =
+      _bindings.TfLiteModelCreateFromFile(modelPath.toCString());
+  Pointer<TfLiteInterpreterOptions> optionalOptions =
+      Pointer<TfLiteInterpreterOptions>.fromAddress(0);
+  Pointer<TfLiteInterpreter> interpreter =
+      _bindings.TfLiteInterpreterCreate(model, optionalOptions);
+  int inputTensorCount =
+      _bindings.TfLiteInterpreterGetInputTensorCount(interpreter);
+  int outputTensorCount =
+      _bindings.TfLiteInterpreterGetOutputTensorCount(interpreter);
+
+  return '''input count = $inputTensorCount
+            output count = $outputTensorCount''';
+}
 
 final DynamicLibrary _dylib = () {
   if (Platform.isMacOS || Platform.isIOS) {
