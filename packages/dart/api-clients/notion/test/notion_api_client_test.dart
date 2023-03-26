7dd09ce99a7e2c0e8da72b1a34d3e54d228765d1
Nick Meinhold <nick@enspyr.co>
2023-03-26 23:46:45 +1100

    Fix test
    I added a PartialUserObject class which is used when a "user" object
    does not have a "type" member, with a comment of where the class fits in.
    
    I added notes to the README on the error code messages we can get from
    the response body (and also how we scrape the Notion docs).
    
    I added test data for a page and added the test data to the response in
    during the test setup to give a nice smoke test.

--- packages/dart/api-clients/notion/test/notion_api_client_test.dart
+++ packages/dart/api-clients/notion/test/notion_api_client_test.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
 import 'package:notion_api_client/notion_api_client.dart';
 import 'package:test/test.dart';
+
+import 'data/page_object.dart';
 
 void main() {
   group('Smoke tests >', () {
@@ -12,18 +15,21 @@ void main() {
     });
 
     test('NotionClient takes a http.Client and uses it to call an endpoint',
-        () {
+        () async {
       final bodyController = StreamController<List<int>>();
+      bodyController.add(Utf8Encoder().convert(pageObjectTestData));
       bodyController.close();
       var response = StreamedResponse(bodyController.stream, 200,
           headers: {'content-type': 'application/json'});
       var mockClient =
           MockClient.streaming((req, stream) => Future.value(response));
       var notion = NotionClient(client: mockClient, token: 'token');
-      notion.getPageProperties(id: 'e93dda7fa5ed4e28ba27e857cd1f6757');
+
+      var pageObject = await notion.getPageProperties(id: '...');
+
       notion.close();
 
-      expect(true, isTrue);
-    }, skip: true); // I think was only half finished, skipping
+      expect(pageObject.id, equals('be633bf1-dfa0-436d-b259-571129a590e5'));
+    });
   });
 }
