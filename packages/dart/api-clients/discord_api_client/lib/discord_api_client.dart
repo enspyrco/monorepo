f10e4d043913e3feebbda7d8d141892c97021b81
Nick Meinhold <nick@enspyr.co>
2023-04-05 01:33:48 +1000

    Add "create message command" example

--- packages/dart/api-clients/discord_api_client/lib/discord_api_client.dart
+++ packages/dart/api-clients/discord_api_client/lib/discord_api_client.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library discord_api_client;
 
 export 'src/application_command.dart';
-
-// TODO: Export any libraries intended for clients of this package.
