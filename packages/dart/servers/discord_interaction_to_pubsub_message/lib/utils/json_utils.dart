d26931d30842cfc3f0480c29cea3802a82bbd321
Nick Meinhold <nick@enspyr.co>
2023-04-06 09:42:12 +1000

    fix(dic): read valid parts  of interaction
    In discord_interaction_to_pubsub_message we now read only valid pars of
    the iinteraction. What is valid has changed now we are using message commands,
    ie, there are no longer 'options'.

--- packages/dart/servers/discord_interaction_to_pubsub_message/lib/utils/json_utils.dart
+++ packages/dart/servers/discord_interaction_to_pubsub_message/lib/utils/json_utils.dart
@@ -1,11 +1,11 @@
 // Extract the incoming value from the json in the body
 import '../typedefs.dart';
 
-Map<String, String> extractInfo(JsonMap json) {
+Map<String, Object?> extractInfo(JsonMap json) {
   var data = json['data'] as JsonMap;
-  var options = data['options'] as JsonList;
-  var option = options.first as JsonMap;
-  var value = option['value'] as String;
+  var options = data['options'] as JsonList?;
+  var option = options?.first as JsonMap?;
+  var value = option?['value'] as String?;
   var applicationId = json['application_id'] as String;
   var token = json['token'] as String;
 
