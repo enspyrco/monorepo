b9c2de78b4e752cb41f7c80a4c7fd0611d088150
Nick Meinhold <nick@enspyr.co>
2023-04-06 23:36:33 +1000

    Add various exceptions to catch specific errors

---
+++ packages/dart/servers/discord_interaction_to_pubsub_message/lib/exceptions/invalid_command_type_exception.dart
@@ -0,0 +1,16 @@
+import 'package:discord_interaction_to_pubsub_message/enums/application_command_type.dart';
+
+class InvalidCommandTypeException implements Exception {
+  const InvalidCommandTypeException({
+    required this.expecting,
+    required this.found,
+  });
+
+  final ApplicationCommandType expecting;
+  final int found;
+
+  @override
+  String toString() {
+    return 'We expected ${expecting.name}, which has value ${expecting.value} but found value $found';
+  }
+}
