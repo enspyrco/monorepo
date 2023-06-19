b84ed8736761583aa62cd54ff7e06dd5f30ff5ef
Nick Meinhold <nick@enspyr.co>
2023-06-20 01:19:59 +1000

    Move astro_test_utils
    Moved to https://github.com/enspyrco/astro_test_utils

--- packages/flutter/utils/astro_test_utils/lib/src/state/mission_report.dart
+++
@@ -1,8 +0,0 @@
-import 'package:astro_types/core_types.dart';
-import 'package:astro_types/state_types.dart';
-
-class MissionReport<T extends AstroState> {
-  MissionReport(this.mission, this.state);
-  final Mission mission;
-  final T state;
-}
