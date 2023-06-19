9803e25e43257f55712f191a7e794edd12675255
Nick Meinhold <nick@enspyr.co>
2023-06-20 03:36:04 +1000

    Move astro to separate enspyrco package (#331)
    Moved to https://github.com/enspyrco/astro_core

--- packages/flutter/utils/astro/lib/astro.dart
+++
@@ -1,7 +0,0 @@
-library astro;
-
-export 'src/core/mission_control.dart';
-export 'src/core/system_checks.dart';
-export 'src/widgets/errors/mission_control_provider_not_found.dart';
-export 'src/widgets/on_state_change_builder.dart';
-export 'src/widgets/utils/progress_indicator_with_message.dart';
--- packages/flutter/utils/astro/test/test-doubles/example_exception.dart
+++
@@ -1,1 +0,0 @@
-class ExampleException implements Exception {}
