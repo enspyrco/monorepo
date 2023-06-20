ce8c584fb9db9dad14368635560e13bbdf8529e5
Nick Meinhold <nick@enspyr.co>
2023-06-21 00:47:41 +1000

    Move astro_inspector_screen to separate repo (#334)
    Moved to https://github.com/enspyrco/astro_inspector_screen

--- packages/flutter/utils/astro_inspector_screen/lib/src/state/viewmodels/app_state_view_view_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:astro_types/json_types.dart';
-
-/// ViewModel class for the [AppStateView] widget
-class AppStateViewViewModel {
-  final JsonMap selectedAppState;
-  final JsonMap previousAppState;
-
-  AppStateViewViewModel(this.selectedAppState, this.previousAppState);
-}
--- packages/flutter/utils/astro_inspector_screen/lib/src/state/viewmodels/mission_history_item_view_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-/// ViewModel class for the [MissionHistoryItem] widget
-class MissionHistoryItemViewModel {
-  final bool isSelected;
-  final List<Widget>? lineageShapes;
-
-  MissionHistoryItemViewModel(this.isSelected, this.lineageShapes);
-}
