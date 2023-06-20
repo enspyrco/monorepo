49916a0f87f73ac3b9a7121436b3fc63b21c52f3
Nick Meinhold <nick@enspyr.co>
2023-06-21 00:26:10 +1000

    Move astro_generators to separate enspyrco repo (#333)
    Moved to https://github.com/enspyrco/astro_generators

--- packages/flutter/utils/astro_generators/README.md
+++
@@ -1,5 +0,0 @@
-# astro_generators
-
-Using Dart's package:build to generate Astro classes.
-
-[Project Notes](https://www.notion.so/enspyrco/Astro-Generators-ce4755005d5443c3b9156a5d9cfbccd6)
--- packages/flutter/utils/astro_generators/lib/astro_builder.dart
+++
@@ -1,7 +0,0 @@
-import 'package:build/build.dart';
-import 'package:source_gen/source_gen.dart';
-
-import 'src/state_implementation_generator.dart';
-
-Builder astroState(BuilderOptions options) =>
-    SharedPartBuilder([StateImplementationGenerator()], 'subclass_generator');
