08b229d74bfe30b257c889e4f5432b72fab142ad
Nick Meinhold <nick@enspyr.co>
2023-06-21 01:22:41 +1000

    Move astro_types to separate repo (#335)
    Moved to https://github.com/enspyrco/astro_types

--- packages/flutter/utils/astro_types/lib/astro_annotations.dart
+++
@@ -1,7 +0,0 @@
-class GeneratedImplementation {
-  const GeneratedImplementation(
-      {this.classNamePrefix = '', this.classNameSuffix = ''});
-
-  final String classNamePrefix;
-  final String classNameSuffix;
-}
