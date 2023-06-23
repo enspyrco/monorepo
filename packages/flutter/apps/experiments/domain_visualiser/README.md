c6f1060f3459e34aeb0bc15b04dd8e9122c7d6db
Nick Meinhold <nick@enspyr.co>
2023-06-24 02:05:07 +1000

    Move domain_visualiser to icebox
    Code saved in project notes:
    https://www.notion.so/enspyrco/domain_visualiser-763ec09b866c4857bc7f8faf0f83d4e4

--- packages/flutter/apps/experiments/domain_visualiser/README.md
+++
@@ -1,5 +0,0 @@
-# domain_visualiser
-
-Draw diagrams to visualise a domain.
-
-[Project Notes](https://www.notion.so/enspyrco/DomainVisualiser-071c2bc6393842b99b4a9d66c711b775)
--- packages/flutter/apps/experiments/domain_visualiser/lib/utils/extensions/class_box_extensions.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/painting.dart';
-
-import '../../models/class_box.dart';
-
-extension ClassBoxExtension on ClassBox {
-  Rect get rect => Rect.fromLTRB(left, top, right, bottom);
-}
