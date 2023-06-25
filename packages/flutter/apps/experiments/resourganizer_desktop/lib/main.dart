1ec9407bccb8fc545a16c99d20d7affdfba2d77c
Nick Meinhold <nick@enspyr.co>
2023-06-25 18:00:48 +1000

    Move resourganizer_desktop to separate repo (#341)
    Moved to https://github.com/enspyrco/reference_converter

--- packages/flutter/apps/experiments/resourganizer_desktop/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:resourganizer_desktop/astro_base.dart';
-
-void main() async {
-  await astroInitialization();
-  runApp(const MaterialApp(home: AstroBase()));
-}
