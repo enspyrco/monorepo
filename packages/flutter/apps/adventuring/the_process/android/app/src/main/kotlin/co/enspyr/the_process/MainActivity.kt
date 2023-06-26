24be2af9c8cc6e675ea1a91aeafcad4910ae3508
Nick Meinhold <nick@enspyr.co>
2023-06-26 13:30:36 +1000

    Move the_process to separate repo
    Moved to https://github.com/enspyrco/the_process

--- packages/flutter/apps/adventuring/the_process/android/app/src/main/kotlin/co/enspyr/the_process/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package co.enspyr.the_process
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/flutter/apps/adventuring/the_process/test/test-data/example_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:astro_types/json_types.dart';
-
-JsonMap storeProjectSectionsActionJson = <String, dynamic>{
-  'list': [
-    {'name': 'name0', 'folderId': 'folderId0', 'useCasesDocId': 'usecase0'},
-    {'name': 'name1', 'folderId': 'folderId1', 'useCasesDocId': 'usecase1'},
-    {'name': 'name2', 'folderId': 'folderId2', 'useCasesDocId': 'usecase2'}
-  ]
-};
