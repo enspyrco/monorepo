c2a5939cf5c2a751fc827affaf10b570a708cf4c
Nick Meinhold <nick@enspyr.co>
2023-06-26 13:37:56 +1000

    Move the_process to separate repo (#342)
    * Move the_process to separate repo
    
    Moved to https://github.com/enspyrco/the_process
    
    * Update matrix.json to remove the_process

--- packages/flutter/apps/adventuring/the_process/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'astro_base.dart';
-
-void main() async {
-  await astroInitialization();
-  runApp(const MaterialApp(home: AstroBase()));
-}
--- packages/flutter/apps/adventuring/the_process/test/test-doubles/example-objects/example_projects.dart
+++
@@ -1,3 +0,0 @@
-import 'package:the_process/projects/models/project_state.dart';
-
-final basicProjectState = ProjectState.initWith(name: 'name');
